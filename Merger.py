1d5f798471c990781d763bf6b8264c45549fc5fb
quentindhn <quentindehaene@gmail.com>
2024-02-22 18:15:14 +0100

    enfin

--- Merger.py
+++ Merger.py
@@ -3,11 +3,11 @@ import os
 
 # Directory containing all CSV files
 script_directory = os.path.dirname(os.path.abspath(__file__))
-directory = os.path.join(script_directory, 'results')
+directory = os.path.join(script_directory, 'results_avec_gaz')
 
 # Output file name for the merged CSV
 outputdir=os.path.join(script_directory, 'resultats_combines')
-output_file = 'Resultats_combines_avec_date.csv'
+output_file = 'Resultats_combines_gaz_75.csv'
 
 # List all CSV files in the directory
 csv_files = [file for file in os.listdir(directory) if file.endswith('.csv')]
@@ -20,7 +20,8 @@ with open(os.path.join(outputdir, output_file), 'w', newline='') as outfile:
     for csv_file in csv_files:
         if csv_file != first_csv:
             with open(os.path.join(directory, csv_file), 'r', newline='') as infile:
-                next(infile)  # Skip the header
+                next(infile)
+                next(infile)   # Skip the header
                 for line in infile:
                     writer.writerow(line.strip().split(','))  # Write data to the output file
         else :
